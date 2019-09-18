a084b7d67fbd5518c4db5e0800b7e0221ab77445
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-09-18 11:19:07 +1000

    Boom

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     public Transform pointPrefab;
-    [Range(10, 100)]
+    Transform[] points;
+    [Range(0, 5000)]
     public int resolution = 10;
     private void Awake()
     {
@@ -13,14 +14,26 @@ public class Graph : MonoBehaviour
         Vector3 scale = Vector3.one * step;
         Vector3 position = Vector3.zero;
 
-        for (int i = 0; i < resolution; i++)
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
         {
             Transform point = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
+            points[i] = point;
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
         }
     }
 }
